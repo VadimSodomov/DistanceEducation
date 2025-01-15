643c26b7e9b5685b132233e88460629ab5fb8e40
elina.biktimirova <Elina.Biktimirova@tatar.ru>
2025-01-15 21:12:01 +0300

    2 | Создать основные контроллеры

---
+++ src/Controller/TestPageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class TestPageController extends AbstractController
+{
+    #[Route('/test/page', name: 'app_test_page')]
+    public function index(): Response
+    {
+        return $this->render('test_page/index.html.twig', [
+            'controller_name' => 'TestPageController',
+        ]);
+    }
+}
