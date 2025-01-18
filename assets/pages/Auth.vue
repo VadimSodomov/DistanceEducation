6e80f4bc7f4322392073466a350f297b84c1bc2b
sofiyachizhkova <sofiyachizhkova@yandex.ru>
2025-01-18 16:40:29 +0300

    5 | Страница входа и регистрации + в app.css изменила цвет фона

--- assets/pages/Auth.vue
+++ assets/pages/Auth.vue
@@ -2,35 +2,43 @@
   <div class="auth-page">
     <div class="auth-form">
       <div class="form-box">
-        <div class="main-title">Авторизация</div>
-        <label for="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          v-model="email"
-          placeholder="Введите email"
-          required
-        />
-
-        <label for="password">Пароль</label>
-        <input
-          type="password"
-          id="password"
-          v-model="password"
-          placeholder="Введите пароль"
-          required
-        />
-
-        <!-- Ссылка "Создать учетную запись" -->
-        <div class="create-account-link">
-          <a href="/register">Создать учетную запись</a>
+        <div class="main-title">
+          <div :class="[{ 'active': isEntry }]" @click="toggleEntry">
+            Вход
+          </div>
+          <div :class="[{ 'active': !isEntry }]" @click="toggleEntry">
+            Регистрация
+          </div>
         </div>
-
-        <button @click="handleLogin" :disabled="isLoading">
+        <div v-if="!isEntry">
+          <input
+              class="auth-input"
+              type=text
+              id="name"
+              v-model="name"
+              placeholder="Введите имя"
+              required
+          />
+        </div>
+        <input
+            class="auth-input"
+            type="email"
+            id="email"
+            v-model="email"
+            placeholder="Введите email"
+            required
+        />
+        <input
+            class="auth-input"
+            type="password"
+            id="password"
+            v-model="password"
+            placeholder="Введите пароль"
+            required
+        />
+        <button class="auth-btn" @click="handleLogin" :disabled="isLoading || disabledBtn">
           {{ isLoading ? 'Загрузка...' : 'Войти' }}
         </button>
-
-        <!-- Блок для отображения ошибки -->
         <div v-if="errorMessage" class="error-message">
           {{ errorMessage }}
         </div>
@@ -45,35 +53,59 @@ import axios from 'axios';
 export default {
   data() {
     return {
+      isEntry: true,
+      name: '',
       email: '',
       password: '',
       isLoading: false,
       errorMessage: '',
     };
   },
+  computed: {
+    disabledBtn() {
+      if (this.isEntry)
+        return !(this.email && this.password && this.password.length >= 5)
+      else
+        return !(this.name && this.email && this.password && this.password.length >= 5)
+    },
+  },
+
   methods: {
+    toggleEntry() {
+      this.isEntry = !this.isEntry;
+    },
+
     async handleLogin() {
       this.isLoading = true;
       this.errorMessage = '';
 
       try {
-        const response = await axios.post('/login', {
-          email: this.email,
-          password: this.password,
-        });
-
+        if (this.isEntry) {
+          await axios.post('/login', {
+            email: this.email,
+            password: this.password,
+          });
+        } else {
+          await axios.post('/register', {
+            name: this.name,
+            email: this.email,
+            password: this.password,
+          });
+        }
         window.location.href = '/';
       } catch (error) {
-        if (error.response && error.response.data && error.response.data.message) {
-          this.errorMessage = error.response.data.message;
+        if (error.response && error.response.data && error.response.data.detail) {
+          this.errorMessage = error.response.data.detail;
         } else {
-          // Если сообщение об ошибке отсутствует, показываем стандартное сообщение
-          this.errorMessage = 'Неверный логин или пароль';
+          if (this.isEntry)
+            this.errorMessage = 'Неверный логин или пароль';
+          else
+            this.errorMessage = 'Ошибка при регистрации';
         }
       } finally {
         this.isLoading = false;
       }
-    },
+    }
   },
 };
 </script>
@@ -84,77 +116,73 @@ export default {
   justify-content: center;
   align-items: center;
   height: 100vh;
-  background-color: #f5f5f5;
-}
 
-.auth-form {
-  width: 100%;
-  max-width: 400px;
-  padding: 20px;
-  background-color: #ffffff;
-  border-radius: 10px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-}
+  .auth-form {
+    width: 100%;
+    max-width: 400px;
+    padding: 20px;
+    background-color: #ffffff;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  }
 
-.form-box {
-  text-align: center;
+  .form-box {
+    text-align: center;
+
+    .auth-input {
+      width: 92%;
+      padding: 10px;
+      border: 1px solid #6D7CF2;
+      border-radius: 14px;
+      margin-bottom: 15px;
+      font-size: 13px;
+    }
+
+    .auth-btn {
+      width: 100%;
+      padding: 10px;
+      background-color: #6D7CF2;
+      color: white;
+      border: none;
+      border-radius: 14px;
+      cursor: pointer;
+      font-size: 18px;
+
+      &:hover {
+        background-color: #8C99FF;
+      }
+
+      &:disabled {
+        background-color: #ABAFCD;
+        cursor: not-allowed;
+      }
+    }
+  }
 }
 
 .main-title {
+  display: flex;
+  justify-content: space-around;
   font-size: 20px;
   margin-bottom: 20px;
   font-weight: 600;
   color: #333;
-}
-
-label {
-  display: block;
-  font-weight: 500;
-  margin-bottom: 5px;
-  color: #555;
-}
-
-input {
-  width: 92%;
-  padding: 10px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  margin-bottom: 15px;
-  font-size: 13px;
-}
-
-.create-account-link {
-  margin-bottom: 15px;
-}
-
-.create-account-link a {
-  color: #190f8b;
-  text-decoration: none;
-  font-size: 13px;
-}
-
-.create-account-link a:hover {
-  text-decoration: underline;
-}
-
-button {
-  width: 100%;
-  padding: 10px;
-  background-color: #46a003;
-  color: white;
-  border: none;
-  border-radius: 5px;
   cursor: pointer;
-  font-size: 14px;
-}
 
-button:hover {
-  background-color: #3d8f04;
-}
+  .active {
+    color: #6D7CF2;
+    position: relative;
 
-button:disabled {
-  background-color: #cccccc;
-  cursor: not-allowed;
+    &:after {
+      content: '';
+      position: absolute;
+      left: 0;
+      bottom: -2px;
+      width: 100%;
+      height: 2px;
+      background-color: #6D7CF2;
+    }
+  }
 }
 
 .error-message {
