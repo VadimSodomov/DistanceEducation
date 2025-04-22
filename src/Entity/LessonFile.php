55a3efd55534b997b0bc328f8a8dcf759e7c0adc
Vadim.Sodomov <vadim.sodomov@yandex.ru>
2025-04-22 08:49:34 +0300

    Добавил сущность для файлов к уроку и миграцию
    Summary:

---
+++ src/Entity/LessonFile.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Repository\LessonFileRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Attribute\Ignore;
+
+#[ORM\Entity(repositoryClass: LessonFileRepository::class)]
+class LessonFile
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: Types::BIGINT)]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'lessonFiles')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Lesson $lesson = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $name = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    #[Ignore]
+    public function getLesson(): ?Lesson
+    {
+        return $this->lesson;
+    }
+
+    public function setLesson(?Lesson $lesson): static
+    {
+        $this->lesson = $lesson;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
